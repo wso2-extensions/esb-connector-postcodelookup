20868fb1456ff1c503fa4f1f1d091aef063e1854
kesavan <ykesan@gmail.com>
2017-11-06 16:44:49 +0530

    Tested with WSO2 EI - 6.1.1.

--- src/test/java/org/wso2/carbon/connector/integration/test/postcodelookup/PostcodelookupConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/postcodelookup/PostcodelookupConnectorIntegrationTest.java
@@ -41,7 +41,9 @@ public class PostcodelookupConnectorIntegrationTest extends ConnectorIntegration
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("postcodelookup-connector-1.0.3-SNAPSHOT");
+        String connectorName = System.getProperty("connector_name") + "-connector-" +
+                System.getProperty("connector_version") + ".zip";
+        init(connectorName);
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
